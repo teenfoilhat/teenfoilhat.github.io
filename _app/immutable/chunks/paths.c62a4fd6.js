116373327658ce8da019055f85bc2b83e2c9184f
teenfoilhat <teenfoilhat@gmail.com>
2023-10-01 21:14:02 -0400

    Updates

---
+++ _app/immutable/chunks/paths.c62a4fd6.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_17rxe2g)==null?void 0:s.base)??"/teenfoilhat.github.io";var e;const a=((e=globalThis.__sveltekit_17rxe2g)==null?void 0:e.assets)??t;export{a,t as b};
