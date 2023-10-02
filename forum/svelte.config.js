b4562f2209c68f04354dec80fa993d09d0680efa
teenfoilhat <teenfoilhat@gmail.com>
2023-10-02 00:31:29 -0400

    bleh

--- forum/svelte.config.js
+++ forum/svelte.config.js
@@ -7,11 +7,8 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: '200.html'
-		}),
-		paths: {
-			base: process.env.NODE_ENV === "production" ? "/teenfoilhat.github.io" : "",
-		}
+			fallback: index.html
+		})
 	}
 };
 
