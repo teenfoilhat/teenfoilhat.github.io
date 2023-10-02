724aab9800dbf66607fec2fe7f71c55efe7eb1d8
teenfoilhat <teenfoilhat@gmail.com>
2023-10-02 01:01:15 -0400

    tired

--- forum/svelte.config.js
+++ forum/svelte.config.js
@@ -1,19 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
-
+import {
+    vitePreprocess
+} from '@sveltejs/kit/vite';
+ 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			fallback: index.html
+    preprocess: vitePreprocess(),
+ 
+    kit: {
+        adapter: adapter({
+			fallback: index.html,
 		}),
-
-		paths: {
-			base: process.env.NODE_ENV === "production" ? "/teenfoilhat.github.io" : "",
-		  },
-	}
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/teenfoil.github.io' : '',
+        }
+    }
 };
-
-export default config;
