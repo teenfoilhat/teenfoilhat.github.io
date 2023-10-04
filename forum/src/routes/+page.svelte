026351a2036cee1080d2195cf43dfb185414064e
teenfoilhat <teenfoilhat@gmail.com>
2023-10-03 21:40:19 -0400

    prerender

--- forum/src/routes/+page.svelte
+++ forum/src/routes/+page.svelte
@@ -1,4 +1,6 @@
-export const prerender = true;
+<script>
+    export const prerender = true;
+</script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
