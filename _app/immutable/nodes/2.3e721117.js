e230634fea4dbf1ad3c11541a69079f63df4cc68
teenfoilhat <teenfoilhat@gmail.com>
2024-01-18 22:40:12 -0500

    Updates

---
+++ _app/immutable/nodes/2.3e721117.js
@@ -0,0 +1,21 @@
+import{s as h,n as i}from"../chunks/scheduler.e108d1fd.js";import{S as u,i as v,g as c,s as f,h as p,y as m,c as H,k as T,a as l,f as r}from"../chunks/index.a21d6cee.js";function x(d){let e,o="<title>TeenFoilHat Forum</title>",a,s,_=`<aside><pre>            
+             //
+            //
+           /  \\
+          /    \\ 
+         /      \\ 
+        /        \\ 
+       —-----------
+      |           |
+      |          /|
+      ( *   *   | /
+      |  L      /
+      \\  ^~^   /\\
+    (   -          )
+  /  |    p          \\
+ /__ /   p         /  \\
+  / /    p        /___|
+
+  TEENFOILHAT FORUM
+
+        </pre></aside> <section><article>Insert Posts Here</article></section>`;return{c(){e=c("head"),e.innerHTML=o,a=f(),s=c("main"),s.innerHTML=_,this.h()},l(t){e=p(t,"HEAD",{"data-svelte-h":!0}),m(e)!=="svelte-ted2iv"&&(e.innerHTML=o),a=H(t),s=p(t,"MAIN",{class:!0,"data-svelte-h":!0}),m(s)!=="svelte-s3z9ud"&&(s.innerHTML=_),this.h()},h(){T(s,"class","svelte-15310x2")},m(t,n){l(t,e,n),l(t,a,n),l(t,s,n)},p:i,i,o:i,d(t){t&&(r(e),r(a),r(s))}}}class g extends u{constructor(e){super(),v(this,e,null,x,h,{})}}export{g as component};
